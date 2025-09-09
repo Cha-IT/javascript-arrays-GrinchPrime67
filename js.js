936c7a9e80221d0c1f3276a10fdbdf2971412cae
GrinchPrime67 <anslind08@gmail.com>
2025-09-09 13:33:35 +0200

    commit

---
+++ js.js
@@ -0,0 +1,13 @@
+//1a
+let primtall = [7, 11, 13, 17, 19, 23, 43, 47, 53, 59, 61, 67];
+
+//1b
+function skrivUtPrimtall() {
+  primtall.forEach((tall) => console.log(tall));
+}
+skrivUtPrimtall();
+document.write(primtall);
+
+//1c
+
+
