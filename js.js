b05723b1c225757d9c1a55b8f21ce30850bee6e2
GrinchPrime67 <anslind08@gmail.com>
2025-09-09 17:24:41 +0200

    commit

--- js.js
+++ js.js
@@ -18,22 +18,60 @@ function allePrimtall() {
 document.write("<br>" + allePrimtall());
 
 
-//oppgave 2 
+//oppgave 2a 
+document.write("<br>" + "<br>");
 let tilfeldigeTall = [];
-let tall = Math.random() * 100
-tall = Math.floor(tall) + 1;
 
-function fyllArray (Liste, nummer) {
+function fyllArray(Liste, nummer) {
     for (let i = 0; i < nummer; i++) {
-        Liste.push(Math.floor(Math.random() * 100) + 1);
+        Liste.push(Math.floor(Math.random() * 100) + 1); 
     }
 }
 
-fyllArray (tilfeldigeTall, 100);
+fyllArray(tilfeldigeTall, 100);
+console.log(tilfeldigeTall); 
+document.write("Tilfeldige tall: " + tilfeldigeTall);
+//VISES BÅDE I KONSOLL OG NETTSIDE FORDI DET ER TØFFT
 
 
+//oppgave 2b
+document.write("<br>" + "<br>");
+let femtiTall = [];
+fyllArray(femtiTall, 50);
+document.write("50 tilfeldige tall: " + femtiTall + "<br>");
 
-console.log(tall);
+//oppgave 2c
+document.write("<br>");
+femtiTall.sort(function(a, b) {
+    return b - a;
+});
+document.write("Sortert synkende: " + femtiTall + "<br>");
+
+//oppgave 2d
+document.write("<br>");
+let største = femtiTall[0];
+let minste = femtiTall[femtiTall.length - 1];
+document.write("Største tall: " + største + "<br>");
+document.write("Minste tall: " + minste + "<br>");
+
+//oppgave 3
+document.write("<br>" + "<br>");
+
+function reverserArray(arr) {
+    let nyArray = [];
+    for (let i = 0; i < arr.length; i++) {
+        nyArray[i] = arr[arr.length - 1 - i];
+    }
+    return nyArray;
+}
+
+let ikkeBokstaver = [1, 2, 3, 4, 5];
+let reversertIkkeBokstaver = reverserArray(ikkeBokstaver); 
+
+document.write("Opprinnelig array: " + ikkeBokstaver + "<br>");
+document.write("Reversert array: " + reversertIkkeBokstaver);
+
+
 
 
 
